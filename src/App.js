3a21e604f45910990d9ef813fa59af79fa055b4e
Nicholas Stathakis <nicholas.stathakis1@gmail.com>
2023-01-04 19:25:14 +1100

    added baseline card views

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import './App.css';
+import ExperienceCard from './components/cards/ExperienceCard';
+import MediaCard from './components/cards/MediaCard';
+import PictureCard from './components/cards/PictureCard';
+import Navbar from './components/Navbar';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      I'm empty! I am now v2!
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <div className="App">
+        <Navbar />
+        <br />
+        <ExperienceCard />
+        <br />
+        <PictureCard />
+        <br />
+        <MediaCard />
+      </div>
+    </ThemeProvider>
   );
 }
 
